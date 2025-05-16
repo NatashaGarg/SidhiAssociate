7d342774098a61e7fca28dac9524f11e144d09b6
Natasha Garg <natashagarg24@gmail.com>
2025-05-16 07:03:57 +0000

    basic changes

---
+++ script.js
@@ -0,0 +1,9 @@
+// Simple JavaScript to handle smooth scrolling to sections
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
